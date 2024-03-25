d9395f7e5d9b813bfb97eb6fc0b1fc2d9f392166
eliudkaruga <eliudkaruga97@gmail.com>
2024-03-25 13:56:50 +0300

    dashboard menu completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Sidebar from "@/components/sidebar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased flex`}>
+        <aside><Sidebar/></aside>
+        <main className="flex-grow">{children}</main>
+      </body>
     </html>
   );
 }
